2f98bc7beec1e3dd0496592e6996b4fd32b93573
Mason Moreland <mazc@protonmail.ch>
2020-08-16 14:03:16 -0700

    Work on doubleEveryOther function

--- 01_intro/01_intro.hs
+++ 01_intro/01_intro.hs
@@ -1,14 +1,23 @@
 module Main where
 
 toDigits :: Integer -> [Integer]
-toDigits 0 = []
-toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
+toDigits x
+  | x > 0 = toDigits (x `div` 10) ++ [x `mod` 10]
+  | otherwise = []
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev 0 = []
-toDigitsRev x = x `mod` 10 : toDigitsRev (x `div` 10)
+toDigitsRev x
+  | x > 0 = x `mod` 10 : toDigitsRev (x `div` 10)
+  | otherwise = []
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x : y : zs) = x * 2 : y : doubleEveryOther zs
+
 
 main :: IO ()
 main = do
   print (toDigits 1234)
   print (toDigitsRev 1234)
+  print (doubleEveryOther [1,2,3,4])
