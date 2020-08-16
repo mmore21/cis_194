f06c15e835ce2235fb98f616976c5b122a8b502c
Mason Moreland <mazc@protonmail.ch>
2020-08-16 13:41:56 -0700

    Finish first problem of hw1

--- 01_intro/01_intro.hs
+++ 01_intro/01_intro.hs
@@ -1,10 +1,14 @@
 module Main where
 
 toDigits :: Integer -> [Integer]
+toDigits 0 = []
+toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
 
 toDigitsRev :: Integer -> [Integer]
+toDigitsRev 0 = []
+toDigitsRev x = x `mod` 10 : toDigitsRev (x `div` 10)
 
 main :: IO ()
-main =
-  | toDigits 1234
-  | toDigitsRev 1234
+main = do
+  print (toDigits 1234)
+  print (toDigitsRev 1234)
